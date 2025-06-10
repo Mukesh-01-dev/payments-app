45a75056d88498ed1573234aee8059158ddc001e
Mukesh Choudhary <mukeshchoudhary33333@gmail.com>
2025-06-10 18:40:33 +0530

    d

---
+++ backend/db.js
@@ -0,0 +1,38 @@
+import mongoose, { Types } from "mongoose";
+
+mongoose.connect("mongodb+srv://mukesh:mongodbpassword>@practise-cluster.4uq6ier.mongodb.net/");
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 10
+    },
+    password: {
+        type: String,
+        minLength: 6,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+    User
+};
